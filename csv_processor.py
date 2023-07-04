79fd23846976e86d1e82b7e0212cf943e3bc046d
Alvaro Zaragoza <alvaro@denzar.com.br>
2023-07-04 10:55:46 -0300

    Steps definition

--- csv_processor.py
+++ csv_processor.py
@@ -5,6 +5,16 @@ Last Update : July, 04, 2023
 Description : Read CSV files, calculate votes, and save results in a new CSV File 
 """
 
+# Steps
+# Create a looger function
+# Create main function
+# Read CSV files into Pandas DataFrames
+# Analyze DataFrames
+# Calculate
+# Create Result CSV file
+
+
+
 
 
 
