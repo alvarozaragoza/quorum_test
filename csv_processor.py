09e858ec5714a537d470b2b1535b4501e2906a4b
Alvaro Zaragoza <alvaro@denzar.com.br>
2023-07-04 11:23:42 -0300

    Logger method created and tested

--- csv_processor.py
+++ csv_processor.py
@@ -5,13 +5,53 @@ Last Update : July, 04, 2023
 Description : Read CSV files, calculate votes, and save results in a new CSV File 
 """
 
-# Steps
+import logging
+import sys
+import platform
+from datetime import datetime
+
 # Create a looger function
+def my_custom_logger(logger_name, level=logging.DEBUG):
+    """
+    Method to return a custom logger with the given name and level
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(level)
+    format_string = "%(asctime)s — line: %(lineno)d — %(message)s"
+    log_format = logging.Formatter(format_string)
+    # Creating and adding the console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(log_format)
+    logger.addHandler(console_handler)
+    # Creating and adding the file handler
+    file_handler = logging.FileHandler(logger_name, mode='a')
+    file_handler.setFormatter(log_format)
+    logger.addHandler(file_handler)
+    return logger
+
 # Create main function
+def main():
+    # Folder separator accordind to OS
+    folder_sep = "\\" if platform.system() == "Windows" else "/"
+
+    # Get current time as string
+    now = datetime.now()
+    current_time = now.strftime("%m_%d_%Y-%H_%M_%S")
+
+    # # Create new log file
+    logger = my_custom_logger(f'log{folder_sep}CSV_Processor_Log_{current_time}.log')
+
+    logger.info("Starting Quorum CSV Processor (version: 1.01)")
+    logger.info("---------------------------------------------")
+
 # Read CSV files into Pandas DataFrames
-# Analyze DataFrames
+# Analyze DataFramescd .
 # Calculate
 # Create Result CSV file
+
+if __name__ == "__main__":
+    main()
+
 
 
 
