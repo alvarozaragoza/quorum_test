72502413e781dad3fcdfc434e64e7ee09dab71e9
Alvaro Zaragoza <alvaro@denzar.com.br>
2023-07-04 10:15:13 -0300

    Starting the CSV_processor program

---
+++ csv_processor.py
@@ -0,0 +1,11 @@
+"""
+Name :        csv_processor
+Author :      Alvaro Zaragoza
+Last Update : July, 04, 2023
+Description : Read CSV files, calculate votes, and save results in a new CSV File 
+"""
+
+
+
+
+
